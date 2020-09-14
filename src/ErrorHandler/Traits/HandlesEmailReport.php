5c4b5e31dd703112d456a8f0488308b89db25ab6
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-09-14 16:42:45 +0800

    UPDATE

--- src/ErrorHandler/Traits/HandlesEmailReport.php
+++ src/ErrorHandler/Traits/HandlesEmailReport.php
@@ -7,7 +7,7 @@ use Freedom\ErrorHandler\Mail\EmailReport;
 
 trait HandlesEmailReport {
 
-    public function shouldReportEmail(Exception $exception){
+    public function shouldReportEmail(Exception $exception) : bool {
         return \Config::get('error-handler.reports.email.enabled',false) === true &&
          !$this->isHttpException($exception) &&
          !$exception instanceof \Illuminate\Database\Eloquent\ModelNotFoundException;
